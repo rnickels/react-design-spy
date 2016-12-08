a0e54a34d39eaa3b17a084e8f94ead191101126b
rnickels <writingcodes4@gmail.com>
2016-12-08 05:34:54 -0700

    got a nap in. realized i don't like those free template assets. deleting them.

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        Robert Nickels | Creative Genius
+      </div>
+    );
+  }
+}
+
+export default Home;
