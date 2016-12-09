afece78ebcbecb8ccb57701ee91eea410b2c9703
rnickels <writingcodes4@gmail.com>
2016-12-09 13:50:00 -0700

    not broken. has my name on it.

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <div className="Footer-header">
+          <h6>Robert Nickels</h6>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
